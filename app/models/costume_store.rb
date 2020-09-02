61868058abc5436a4390a200f1b1bd69d6c99614
Samantha Aleman <samantha.aleman25@gmail.com>
2020-09-02 01:38:48 +0000

    Automatically backed up by Learn

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,5 +1,5 @@
 # Create your CostumeStore class here
-class CostumeStore < ActiveRecord::Migration[5.1]
+class CostumeStore < ActiveRecord::Base
   
   def change 
     create_table :costume_stores do |t|
