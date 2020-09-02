4676a0bc144272ab2f44f5e62d70c8b71626b96f
Samantha Aleman <samantha.aleman25@gmail.com>
2020-09-02 01:40:31 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,15 @@
 # Create your costume_stores migration here
+class CostumeStore < ActiveRecord::Base
+  
+  def change 
+    create_table :costume_stores do |t|
+      t.string :name 
+      t.string :location
+      t.integer :number_of_costumes
+      t.integer :number_of_employees
+      t.boolean :in_business
+      t.timestamp :opening 
+      t.timestamp :opening 
+    end 
+  end 
+end 
