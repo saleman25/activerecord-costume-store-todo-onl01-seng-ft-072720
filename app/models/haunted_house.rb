2598fa35edc91895dd424a4b1fb52c96a8dfde16
Samantha Aleman <samantha.aleman25@gmail.com>
2020-09-02 01:41:30 +0000

    Automatically backed up by Learn

--- app/models/haunted_house.rb
+++ app/models/haunted_house.rb
@@ -1,16 +1,3 @@
 # Create your HauntedHouse class here
 class HauntedHouse < ActiveRecord::Base
-  
-  def change
-    create_table :haunted_houses do |t|
-      t.string :name 
-      t.string :location
-      t.string :theme
-      t.integer :price
-      t.boolean :family_friendly
-      t.timestamp :opening
-      t.timestamp :closing
-      t.string :long_description
-    end 
-  end 
 end 
